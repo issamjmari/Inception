01a531850738b9deca18904632c7cafa4fa44819
Issam Jmari <ijmari@e1r5p7.1337.ma>
2023-05-11 18:00:32 +0100

    database working properly

--- srcs/requirements/mariadb/mariaScript/script.sh
+++ srcs/requirements/mariadb/mariaScript/script.sh
@@ -13,8 +13,8 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wp_database ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS 'issam'@'%' IDENTIFIED BY '1234' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON wp_database.* TO 'issam'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
-
 mysql < db1.sql
-exec mysqld_safe
+service mysql stop
+exec mysqld
+# /etc/mysql/mariadb.conf.d
